abbb71b474b7feff51db78f665efc4ea2696a93a
Vitória Canon <98993613+canonvitoria@users.noreply.github.com>
2023-02-13 20:13:27 -0300

    HotSite-IOS

---
+++ terminal.js
@@ -0,0 +1,29 @@
+function meuEscopo() {
+    const form = document.querySelector('.form');
+    const resultado = document.querySelector('.resultado');
+
+    const pessoas = [];
+
+    function recebeEventForm(event) {
+        event.preventDefault();
+
+       const nome = form.querySelector('.nome');
+       const sobrenome = form.querySelector('.sobrenome');
+       const email = form.querySelector('.email');
+       const telefone = form.querySelector('.telefone');
+
+       pessoas.push({
+        nome: nome.value,
+        sobrenome: sobrenome.value,
+
+        telefone: telefone.value
+       });
+
+       console.log(pessoas);
+
+       resultado.innerHTML = `<p>${nome.value} ${sobrenome.value} seus dados foram cadastrados com sucesso.</p>`
+    }
+
+    form.addEventListener('submit', recebeEventForm);
+}
+meuEscopo();
